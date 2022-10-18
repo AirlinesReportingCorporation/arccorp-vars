e4f428379fa3c0beb49174257d3b36c5c62ee9c0
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-10-18 15:40:33 -0400

    add product icons

--- src/components/ProductIcons.jsx
+++ src/components/ProductIcons.jsx
@@ -11,37 +11,37 @@ class ProductIcons extends Component {
         class="bg-section data-global-section lazy"
         data-bg="https://www2.arccorp.com/globalassets/destination-gateway/dg-bg-2.png"
       >
-        <div class="data-overview" id="overview">
-          <div class="dg-icon-container">
+        <div class="data-overview" id={this.props.id}>
+          <div class="product-icon-container">
             <div class="text-center">
               <div class="row">
                 <div class="col-lg-12">
-                  <div className="dg-icon-title text-center" style={{marginTop: "60px"}}>
+                  <div
+                    className="product-icon-title text-center"
+                    style={{ marginTop: "60px" }}
+                  >
                     {this.props.copy}
                   </div>
                 </div>
               </div>
 
               <div class="row ss-row">
-              {this.props.prodIcons ? 
-              this.props.prodIcons.map((prodIcons, i) => (
-                <div class="col-lg-6">
-                  <div class="ss-system-info-item d-flex align-items-center">
-                    <div class="dg-icon-img">
-                      <div class="lazyload-wrapper">
-                        <img
-                          class="lazy"
-                          src={prodIcons.image}
-                        />
-                      </div>
-                    </div>
+                {this.props.prodIcons
+                  ? this.props.prodIcons.map((prodIcons, i) => (
+                      <div class="col-lg-6">
+                        <div class="ss-system-info-item d-flex align-items-center">
+                          <div class="product-icon-img">
+                            <div class="lazyload-wrapper">
+                              <img class="lazy" src={prodIcons.image} />
+                            </div>
+                          </div>
 
-                    <div class="dg-icon-copy">{prodIcons.copy}</div>
-                  </div>
-                </div>
-              )) :""
-                }
-                </div>
+                          <div class="product-icon-copy">{prodIcons.copy}</div>
+                        </div>
+                      </div>
+                    ))
+                  : ""}
+              </div>
             </div>
           </div>
         </div>
---
+++ src/stories/ProductIcons.stories.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import ProductIcons from "../components/ProductIcons";
+import "./main.scss";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/ProductIcons",
+  component: ProductIcons,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: "color" },
+  // },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <ProductIcons {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  id: "product-icons",
+  copy: "Testing Product Icons Copy",
+  prodIcons: [
+    {
+    image: "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-1.png",
+    alt: "Strategic Planning",
+    copy: "Strategic Planning",
+  },
+    {
+    image: "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-2.png",
+    alt: "Foresight and Planning",
+    copy: "Foresight and Planning",
+    },
+    {
+    image: "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-3.png",
+    alt: "Competitive Advantages",
+    copy: "Competitive Advantages",
+    },
+    {
+      image: "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-4.png",
+      alt: "Credibility with Stakeholders",
+      copy: "Credibility with Stakeholders",
+      },
+  ]
+ 
+};
