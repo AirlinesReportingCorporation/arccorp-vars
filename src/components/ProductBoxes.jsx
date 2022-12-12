e53bd926a2208ca83cadb87fbb70c04c4bb0315c
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-12-12 13:01:20 -0500

    work on card css

--- src/components/ProductBoxes.jsx
+++ src/components/ProductBoxes.jsx
@@ -8,23 +8,27 @@ class ProductBoxes extends Component {
   }
 
   render() {
+    {console.log(this.props.prodBoxes)}
     return (
-      <div className="row">
+      <div className="container">
+        <div className="row">
         {this.props.prodBoxes
                   ? this.props.prodBoxes.map((box, i) => (
-        <div classname="col-lg-4">
-          <div classname="data-icon-inner data-icon-number-inner text-center" style="padding-bottom: 35px">
-            <div classname="data-icon-number">{box.number}</div>
-            <div classname="data-icon-grid-text">{box.text}</div>
-            <div classname="data-icon-grid-label">{box.label}</div>
-            <div style="margin-top: 1.5rem">
-              {box.links ? (box.links.length > 1 ? box.links.map((link, i) => (
-                 <a target="_blank" href={link.url} classname="link-download">{link.cta}<i classname="fas fa-chevron-right"></i></a>
-              )) : <a target="_blank" href={link.url} classname="link-download" style="margin-right: 1rem">{link.cta}<i classname="fas fa-chevron-right"></i></a> ) : "" }
+        <div className="col-lg-4">
+          <div className="data-icon-inner data-icon-number-inner text-center" style={box.links ? {paddingBottom: "35px"} : {paddingBottom: ''}}>
+            <div className="data-icon-number">{box.number}</div>
+            <div className="data-icon-grid-text">{box.text}</div>
+            <div className="data-icon-grid-label">{box.label}</div>
+            {console.log(box.links ? 'true' : 'false')}
+            <div>
+              {box.links ? (box.links.map((link, i) => (
+                 <a target="_blank" href={link.url} className="link-download">{link.cta}<i className="fas fa-chevron-right"></i></a>
+              ))) : "" }
             </div>
           </div>
         </div>
         )) : "" }
+      </div>
       </div>
     ) 
   }
--- src/stories/ProductBoxes.stories.jsx
+++ src/stories/ProductBoxes.stories.jsx
@@ -14,18 +14,42 @@ export default {
   };
   
   // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-  const Template = (args) => (
-    <div className="full-width">
-      <ProductBoxes {...args} />
-    </div>
-  );
+  const Template = (args) => <ProductBoxes {...args} />;
   
   export const Primary = Template.bind({});
   // More on args: https://storybook.js.org/docs/react/writing-stories/args
-  Primary.args = 
+  Primary.args = {
+      prodBoxes :
     [
-        {number: "1", text: "Review", label: "Application & Manual", links: [{url: "/globalassets/ndc/direct-connect-program-application_v8.pdf", cta: "Application"}, {url: "/globalassets/ndc/Direct-Connect-Program-Manual.pdf", cta: "Manual"}]},
-        {number: "2", text: "Initiate", label: "Discovery Call with ARC", links: [{url: "mailto:NDC@arccorp.com", cta: "Schedule"}]},
-        {number: "3", text: "Determine", label: "Transaction Business Rules", links: ""},
+        {
+            number: "1", 
+            text: "Review", 
+            label: "Application & Manual", 
+            links: [
+                {
+                    url: "/globalassets/ndc/direct-connect-program-application_v8.pdf", 
+                    cta: "Application"}, 
+                {
+                    url: "/globalassets/ndc/Direct-Connect-Program-Manual.pdf", 
+                    cta: "Manual"
+                }
+            ]
+        },
+        {
+            number: "2", 
+            text: "Initiate", 
+            label: "Discovery Call with ARC", 
+            links: [
+                {
+                    url: "mailto:NDC@arccorp.com", 
+                    cta: "Schedule"
+                }
+            ]
+        },
+        {
+            number: "3",
+            text: "Determine",
+            label: "Transaction Business Rules"
+        },
     ]
-  ;
+};
