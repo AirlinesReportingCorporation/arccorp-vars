431450f15e8e15f16ec0f676589faeec55ca6dc9
BreeArmstrong <breearmstrong@ARCMACBOOK31.local>
2023-05-15 14:14:44 -0400

    update icon card to be both component and section

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -27,23 +27,84 @@ class IconCard extends Component {
   // Adjust the classes to be independant of the sales stats page
 
   render() {
-    return (
-      <div class="row ss-row icon-card-row">
+    return this.props.type == "component" ? (
+      <div class="row arc-icon-card-row">
         {this.props.iconCards
           ? this.props.iconCards.map((card, i) => (
               <div class="col-lg-6">
-                <div style={card.cardStyle} class="ss-system-info-item icon-card d-flex align-items-center">
-                  <div class="product-icon-img icon-card-image">
+                <div
+                  style={card.cardStyle}
+                  class="arc-icon-card d-flex align-items-center"
+                >
+                  <div class="arc-icon-card-image">
                     <div class="lazyload-wrapper">
-                      <img class="lazy" style={card.imageStyle} src={card.image} />
+                      <img
+                        class="lazy"
+                        style={card.imageStyle}
+                        src={card.image}
+                      />
                     </div>
                   </div>
 
-                  <div class="product-icon-copy icon-card-copy" style={card.copyStyle} >{card.copy}</div>
+                  <div
+                    class="arc-icon-card-copy"
+                    style={card.copyStyle}
+                  >
+                    {card.copy}
+                  </div>
                 </div>
               </div>
             ))
           : ""}
+      </div>
+    ) : (
+      <div
+        className={
+          this.props.class
+            ? this.props.class + " bg-section lazy"
+            : "bg-section lazy"
+        }
+        style={{ backgroundImage: this.props.bg ? this.props.bg : "" }}
+        data-bg={this.props.bg}
+      >
+        <div class={this.props.bg} id={this.props.id}>
+          <div class="product-icon-container">
+            <div class="text-center">
+              <div class="row">
+                {this.props.copy ? (
+                  <div class="col-lg-12">
+                    <div
+                      className="product-icon-title text-center"
+                      style={{ marginTop: "60px" }}
+                    >
+                      {this.props.copy}
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+
+              <div class="row arc-icon-card-row">
+                {this.props.prodIcons
+                  ? this.props.prodIcons.map((prodIcons, i) => (
+                      <div class="col-lg-6">
+                        <div class="arc-icon-card d-flex align-items-center">
+                          <div class="arc-icon-card-image">
+                            <div class="lazyload-wrapper">
+                              <img class="lazy" src={prodIcons.image} />
+                            </div>
+                          </div>
+
+                          <div class="arc-icon-card-copy">{prodIcons.copy}</div>
+                        </div>
+                      </div>
+                    ))
+                  : ""}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/stories/IconCard.stories.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+
+import IconCard from "../components/IconCard";
+import "./main.scss";
+import "../scss/products.scss";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/IconCard",
+  component: IconCard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: "color" },
+  // },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <IconCard {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  class: "",
+  bg: "https://www2.arccorp.com/globalassets/destination-gateway/dg-bg-2.png",
+  id: "product-icons",
+  copy: "Testing Product Icons Copy",
+  prodIcons: [
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-1.png",
+      alt: "Strategic Planning",
+      copy: "Strategic Planning",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-2.png",
+      alt: "Foresight and Planning",
+      copy: "Foresight and Planning",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-3.png",
+      alt: "Competitive Advantages",
+      copy: "Competitive Advantages",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-4.png",
+      alt: "Credibility with Stakeholders",
+      copy: "Credibility with Stakeholders",
+    },
+  ],
+};
+
+export const ColorBackground = Template.bind({});
+ColorBackground.args = {
+  class: "bg-color-teal",
+  bg: "",
+  id: "product-icons",
+  copy: "Testing Product Icons Copy",
+  prodIcons: [
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-1.png",
+      alt: "Strategic Planning",
+      copy: "Strategic Planning",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-2.png",
+      alt: "Foresight and Planning",
+      copy: "Foresight and Planning",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-3.png",
+      alt: "Competitive Advantages",
+      copy: "Competitive Advantages",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-4.png",
+      alt: "Credibility with Stakeholders",
+      copy: "Credibility with Stakeholders",
+    },
+  ],
+};
+
+export const Component = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Component.args = {
+  class: "",
+  bg: "https://www2.arccorp.com/globalassets/destination-gateway/dg-bg-2.png",
+  id: "product-icons",
+  type: "component",
+  copy: "Testing Product Icons Copy",
+  iconCards: [
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-1.png",
+      alt: "Strategic Planning",
+      copy: "Strategic Planning",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-2.png",
+      alt: "Foresight and Planning",
+      copy: "Foresight and Planning",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-3.png",
+      alt: "Competitive Advantages",
+      copy: "Competitive Advantages",
+    },
+    {
+      image:
+        "https://www2.arccorp.com/globalassets/destination-gateway/dg-icon-4.png",
+      alt: "Credibility with Stakeholders",
+      copy: "Credibility with Stakeholders",
+    },
+  ],
+};
+
