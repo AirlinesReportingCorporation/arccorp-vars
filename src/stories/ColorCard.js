ec5abd60aa1cada1b27407375f380d1264d05503
BreeArmstrong <breearmstrong@ARCMACBOOK31.local>
2023-06-06 15:58:19 -0400

    update to color caards

--- src/stories/ColorCard.js
+++ src/stories/ColorCard.js
@@ -5,8 +5,12 @@ import React from "react";
 let darkColors = ["Tarmac", "Concourse", "Nightshade","Autopilot", "Landing", "Fuselage", "Teal", "Now Boarding"]
 export default function ColorCard(props) {
     return (
-          <div className={props.class}>
+          <div className={"sb-color-card-outer " + props.class}>
             <p className="type-font-semibold" style={{color: (darkColors.includes(props.name) ? "white" : "black")}}>{props.name}</p>
+            <div className="arc-pt-30">
+            <span className="type-font-semibold" style={{color: (darkColors.includes(props.name) ? "white" : "black")}}>Class:</span> <br/>
+            <span style={{color: (darkColors.includes(props.name) ? "white" : "black")}}>{props.class} {props.alias != "" ? "or bg-color-" + props.alias : ""}</span>
+            </div>
           </div>
       );
     
