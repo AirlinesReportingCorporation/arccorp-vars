df54b0797e0cce1403e1c0eb456595ea0fb7dbc7
BreeArmstrong <brianna.m.armstrong@gmail.com>
2023-06-27 15:54:30 -0400

    add alert to copy function

--- src/stories/ColorCard.js
+++ src/stories/ColorCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 // Add auto text color change if the background is dark enough.
 // Add styles using the new spacing options.
@@ -13,14 +13,16 @@ let darkColors = [
   "Teal",
   "Now Boarding",
 ];
+
 export default function ColorCard(props) {
+
   let fontColor = darkColors.includes(props.name) ? "white" : "black";
   return (
     <div className={"sb-color-card-outer " + props.class}>
       <span className="type-font-semibold" style={{ color: fontColor }}>
         {props.name}
       </span>
-      <br/>
+      <br />
       <span style={{ color: fontColor }}>{props.hex}</span>
       <div className="arc-pt-30">
         <span className="type-font-semibold" style={{ color: fontColor }}>
@@ -62,13 +64,37 @@ export default function ColorCard(props) {
         style={{ color: fontColor }}
       >
         Notes:
-        <br/>
+        <br />
         <p>{props.note}</p>
       </div>
 
       <div className="d-flex">
-        <CopyToClipboard text={props.hex}><a className="ctaBtn" style={{border: "1px solid #2a2b2c", marginLeft: "auto", marginRight: "auto"}}>Copy Hex</a></CopyToClipboard>
-        <CopyToClipboard text={props.class}><a className={props.name == "Teal" ? "ctaBtn sbBtn-secondary" : "ctaBtn ctaBtn--main-outline"} style={{marginLeft: "auto", marginRight: "auto"}}>Copy Class</a></CopyToClipboard>
+        <CopyToClipboard text={props.hex}>
+          <a
+            onClick={() => alert("Copied hex value " + props.hex)}
+            className="ctaBtn"
+            style={{
+              border: "1px solid #2a2b2c",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            Copy Hex
+          </a>
+        </CopyToClipboard>
+        <CopyToClipboard text={props.class}>
+          <a
+          onClick={() => alert("Copied class: " + props.class)}
+            className={
+              props.name == "Teal"
+                ? "ctaBtn sbBtn-secondary"
+                : "ctaBtn ctaBtn--main-outline"
+            }
+            style={{ marginLeft: "auto", marginRight: "auto" }}
+          >
+            Copy Class
+          </a>
+        </CopyToClipboard>
       </div>
     </div>
   );
