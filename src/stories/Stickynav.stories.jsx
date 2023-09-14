e36ee3a13f3689cf5e7a4c514dc4f85912d5dc79
Ian Fajardo <ifajardo@arccorp.com>
2023-09-14 10:48:13 -0400

    update stickynav story with new pageLink

--- src/stories/Stickynav.stories.jsx
+++ src/stories/Stickynav.stories.jsx
@@ -80,6 +80,7 @@ const Template = (args) => (
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
+  pageLink: "#top",
   className: "bg-color-teal",
   title: "Destination Gateway",
   links: [
