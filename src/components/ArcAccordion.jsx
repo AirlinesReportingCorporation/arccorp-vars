1276b49aa002d936e9bff1387367938ad8d31285
Bree Armstrong <barmstrong@arccorp.com>
2024-03-18 13:34:02 -0400

    update to add multiple accordions in a story

--- src/components/ArcAccordion.jsx
+++ src/components/ArcAccordion.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 export default function ARCAccordion(props) {
-    
   const [isShowing, setShowing] = useState(false);
 
   useEffect(() => {
@@ -13,64 +12,59 @@ export default function ARCAccordion(props) {
     }
   }, [props.showing]);
 
-
   const toggleShow = () => {
     setShowing(!isShowing);
   };
 
   return (
-    <div class="row">
-      <div class="col-lg-12 show">
-        <div
-          className={
-            (isShowing 
-              ? "accordionRow active alert-box"
-              : "accordionRow alert-box") +
-            (props.highlighted ? " fraud-highlight" : "")
-          }
-        >
-          <div className="accordionRowTop">
-            <div
-              className={
-                isShowing 
-                  ? "accordionRowStart container-fluid active"
-                  : "accordionRowStart container-fluid"
-              }
-              onClick={toggleShow}
-            >
-              <div className="row align-items-center">
-                <div className="col-11">
-                  <div className="d-flex flex-column flex-lg-row">
-                    <div className="">
-                      <div className="accordionName" id={props.anchor}>
-                        {props.name}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-1">
-                  <div>
-                    <div className="apExpand">
-                      <div
-                        className={isShowing  ? "fs-close" : "fs-open"}
-                      ></div>
+    <div class="col-lg-12 show">
+      <div
+        className={
+          (isShowing
+            ? "accordionRow active alert-box"
+            : "accordionRow alert-box") +
+          (props.highlighted ? " fraud-highlight" : "")
+        }
+      >
+        <div className="accordionRowTop">
+          <div
+            className={
+              isShowing
+                ? "accordionRowStart container-fluid active"
+                : "accordionRowStart container-fluid"
+            }
+            onClick={toggleShow}
+          >
+            <div className="row align-items-center">
+              <div className="col-11">
+                <div className="d-flex flex-column flex-lg-row">
+                  <div className="">
+                    <div className="accordionName" id={props.anchor}>
+                      {props.name}
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div
-              className={
-                isShowing 
-                  ? "accordionRowInfo show animated slideInDown"
-                  : "accordionRowInfo"
-              }
-            >
-              <div className="row no-gutters">
-                <div className="col">
-                  <div className="apSection apSectionMargin">
-                    <div className="apSectionBottom">{props.body}</div>
+              <div className="col-1">
+                <div>
+                  <div className="apExpand">
+                    <div className={isShowing ? "fs-close" : "fs-open"}></div>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className={
+              isShowing
+                ? "accordionRowInfo show animated slideInDown"
+                : "accordionRowInfo"
+            }
+          >
+            <div className="row no-gutters">
+              <div className="col">
+                <div className="apSection apSectionMargin">
+                  <div className="apSectionBottom">{props.body}</div>
                 </div>
               </div>
             </div>
--- src/stories/ARCAccordion.stories.jsx
+++ src/stories/ARCAccordion.stories.jsx
@@ -25,18 +25,51 @@ export default {
     },
     showing: {
       description: "if it is showing or not",
-      control: 'boolean',
+      control: "boolean",
     },
   },
 };
 
 const Template = (args) => (
   <div className="full-width">
-    <ARCAccordion {...args} />
+    <div className="row">
+      <ARCAccordion {...args} />
+    </div>
   </div>
 );
 
 export const Primary = Template.bind({});
+
+export const Grouped = () => {
+  const groupedAccrodion = Array("list of accordions", [
+    {
+      name: "I am an Accordion",
+      body: "Hello!",
+      showing: false,
+    },
+    {
+      name: "I am an Accordion 1",
+      body: "Hello!",
+      showing: false,
+    },
+    {
+      name: "I am an Accordion 2",
+      body: "Hello!",
+      showing: false,
+    },
+  ]);
+
+  return (
+    <div className="full-width">
+      <div className="row">
+        {groupedAccrodion[1].map(function (accordion) {
+          console.log(accordion);
+          return <ARCAccordion {...accordion} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 Primary.args = {
   name: "I am an Accordion",
